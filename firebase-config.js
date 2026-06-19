42819777d8411c158cae1153b3aebc9b97d7263c
MingoTea <93476164+MingoTea@users.noreply.github.com>
2026-06-19 23:20:39 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA3y2g6K63Pu_HrGgwhC804QcvZLgHxCOg",
+  authDomain: "choiunonha.firebaseapp.com",
+  projectId: "choiunonha",
+  storageBucket: "choiunonha.firebasestorage.app",
+  messagingSenderId: "474956613479",
+  appId: "1:474956613479:web:d3772dd2cc2667f2fa91c2",
+  measurementId: "G-XGD1B73130"
+};
